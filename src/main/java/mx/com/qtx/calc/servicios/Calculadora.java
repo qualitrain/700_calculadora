3f282f1d8e94980200e300ccfdf620693531dc20
Alex Cruz <acruz@qualitrain.com.mx>
2024-09-13 12:47:52 -0600

    Firmas de requerimiento ok

--- src/main/java/mx/com/qtx/calc/servicios/Calculadora.java
+++ src/main/java/mx/com/qtx/calc/servicios/Calculadora.java
@@ -5,9 +5,19 @@ public class Calculadora {
 		double resultado =  (num1.doubleValue() + num2.doubleValue());
 		return resultado;
 	}
+	
 	public static <T extends Number> double restar(T num1, T num2) {
 		double resultado =  (num1.doubleValue() - num2.doubleValue());
 		return resultado;
 	}
+	
+	public static <T extends Number> double multiplicar(T num1, T num2) {
+		
+		return 0;
+	}
 
+	public static <T extends Number> double dividir(T num1, T num2) {
+		
+		return 0;
+	}
 }
